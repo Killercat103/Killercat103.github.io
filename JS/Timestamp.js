a23d0c7e15d83bc8309d9a33a7c1d4d7cf790647
Killercat103 <75360760+Killercat103@users.noreply.github.com>
2021-03-16 13:41:12 +0100

    Update Timestamp.js

--- JS/Timestamp.js
+++ JS/Timestamp.js
@@ -1,16 +1,20 @@
-today = new Date();
+function calctime {
+   today = new Date();
 
-var date = String(today.getDate()).padStart(2, "0") + "/" + String(today.getMonth() + 1).padStart(2, "0") + "/" + today.getFullYear();
+   var date = String(today.getDate()).padStart(2, "0") + "/" + String(today.getMonth() + 1).padStart(2, "0") + "/" + today.getFullYear();
 
-var hours = today.getHours();
-var time = hours + ":" + String(today.getMinutes()).padStart(2, "0") + ":" + String(today.getSeconds()).padStart(2, "0");
-hours = parseInt(hours);
-if (hours < 12) {
-   time = time + " AM";
-} else if (hours > 12) {
-  hours = hours - 12;
-  time = time + " PM";
+   var hours = today.getHours();
+   var time = hours + ":" + String(today.getMinutes()).padStart(2, "0") + ":" + String(today.getSeconds()).padStart(2, "0");
+   hours = parseInt(hours);
+   if (hours < 12) {
+      time = time + " AM";
+   } else if (hours > 12) {
+      hours = hours - 12;
+      time = time + " PM";
+   }
+   hours = toString(hours);
+   timestamp = time + " " + date;
+   document.getElementById("date").innerHTML = timestamp;
 }
-hours = toString(hours);
-timestamp = time + " " + date;
-document.getElementById("date").innerHTML = timestamp;
+
+var counter=setInterval(calctime, 1000);
