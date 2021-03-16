fa8a4d6fe838fe933c613c957f7e6c4fcf3ba578
Killercat103 <75360760+Killercat103@users.noreply.github.com>
2021-03-16 10:25:42 +0100

    Update Timestamp.js

--- JS/Timestamp.js
+++ JS/Timestamp.js
@@ -1,4 +1,6 @@
 today = new Date();
 var date = String(today.getDate()).padStart(2, "0") + "/" + String(today.getMonth() + 1).padStart(2, "0") + "/" + today.getFullYear();
 
-document.getElementById("date").innerHTML = "Timestamp: " + "(time)" + " " + date;
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+document.getElementById("date").innerHTML = "Timestamp: " + time + " " + date;
