b482768f5e90873ced6126d89d79990037d64dab
Killercat103 <75360760+Killercat103@users.noreply.github.com>
2021-03-16 13:00:46 +0100

    Update Timestamp.js

--- JS/Timestamp.js
+++ JS/Timestamp.js
@@ -3,10 +3,9 @@ today = new Date();
 var date = String(today.getDate()).padStart(2, "0") + "/" + String(today.getMonth() + 1).padStart(2, "0") + "/" + today.getFullYear();
 
 var hours = today.getHours();
-hours = parseInt(hours);
-if hours < 12 {
+if (hours < 12) {
   hours = hours + "AM";
-} else if hours > 12 {
+} else if (hours > 12) {
   hours = hours - 12 + "PM";
 }
 var time = hours + ":" + today.getMinutes() + ":" + today.getSeconds();
