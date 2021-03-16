867d308160c9f8c1bc71593592d9f684a8382012
Killercat103 <75360760+Killercat103@users.noreply.github.com>
2021-03-16 13:15:07 +0100

    Update Timestamp.js

--- JS/Timestamp.js
+++ JS/Timestamp.js
@@ -5,11 +5,11 @@ var date = String(today.getDate()).padStart(2, "0") + "/" + String(today.getMont
 var hours = today.getHours();
 var time = hours + ":" + String(today.getMinutes()).padStart(2, "0") + ":" + String(today.getSeconds()).padStart(2, "0");
 if (hours < 12) {
+   time = time + " AM";
   timestamp = time + " " + date;
-  timestamp = timestamp + " AM";
 } else if (hours > 12) {
   hours = hours - 12;
+  time = time + " PM";
   timestamp = time + " " + date;
-  timestamp = timestamp + " PM";
 }
 document.getElementById("date").innerHTML = timestamp;
