0b56791dbe50115453850949e0d0e7b5b43a7241
Killercat103 <75360760+Killercat103@users.noreply.github.com>
2021-03-16 16:57:19 +0100

    Update Timestamp.js

--- JS/Timestamp.js
+++ JS/Timestamp.js
@@ -1,15 +1,31 @@
 function calctime() {
-   today = new Date();
-   var date = String(today.getDate()).padStart(2, "0") + "/" + String(today.getMonth() + 1).padStart(2, "0") + "/" + today.getFullYear();
-   var time = ":" + String(today.getMinutes()).padStart(2, "0") + ":" + String(today.getSeconds()).padStart(2, "0");
-   var hours = today.getHours();
-   hours = parseInt(hours);
-   if (hours < 12) {
-      time = time + " AM";
-   } else if (hours > 12) {
-      hours = hours - 12;
-      time = time + " PM";
-   }
+	today = new Date();
+	var date = String(today.getDate()).padStart(2, "0") + "/" + String(today.getMonth() + 1).padStart(2, "0") + "/" + today.getFullYear();
+	var time = ":" + String(today.getMinutes()).padStart(2, "0") + ":" + String(today.getSeconds()).padStart(2, "0");
+	var hours = today.getHours();
+	hours = parseInt(hours);
+
+	function AM() {
+		time = time + " AM";
+	}
+	function PM() {
+		hours = hours - 12;
+		time = time + " PM";
+	}
+
+	if (hours < 12) {
+		if (hours === 00) {
+			hours = 12
+			AM()
+		} else {
+			AM()
+		}
+	} else if (hours === 12) {
+		time = time + " PM"
+	} else if (hours > 12) {
+		PM()
+	}
+
    hours = hours.toString();
    time = hours + time;
    timestamp = time + " " + date;
